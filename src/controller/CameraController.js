8b5ef1ed03ab807c51c1bc354a72441929e9eba4
Gabriel <gabriel.sappio02@gmail.com>
2022-10-14 12:10:42 -0300

    In-camera adjustment, using Canvas

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,15 +1,40 @@
-class CameraController {
+export class CameraController {
 
     constructor(videoEl) {
         this._videoEl = videoEl
 
         navigator.mediaDevices.getUserMedia({ video: true }).then(screenStream => {
-            this._screenStream = screenStream
+
+            this._screenStream = screenStream;
             this._videoEl.srcObject = screenStream;
             this._videoEl.play()
         }).catch(err => {
             console.error(err)
         })
+    }
+
+    stop(){
+
+        screenStream.getTracks().forEach(track=>{
+            track.stop();
+        });
+
+
+    }
+
+    takePicture(mimeType = 'image/png'){
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videosHeight);
+        canvas.setAttribute('width', this._videoEl.videosWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
+
     }
 
 }
